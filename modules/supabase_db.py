b38d4635cec4daa1e74c35ce6a2dfa0328f846ae
sattrioenn-sudo <sattrioenn@gmail.com>
2026-04-03 21:59:11 +0700

    Update supabase_db.py

--- modules/supabase_db.py
+++ modules/supabase_db.py
@@ -1,1 +1,15 @@
-# File untuk konfigurasi koneksi ke Supabase
+import os
+from supabase import create_client, Client
+
+# Kita gunakan os.environ agar lebih aman saat di-deploy ke Vercel nanti
+# URL dan KEY ini bisa kamu dapatkan di Settings > API di dashboard Supabase-mu
+SUPABASE_URL = os.environ.get("SUPABASE_URL")
+SUPABASE_KEY = os.environ.get("SUPABASE_SERVICE_KEY")
+
+def get_client() -> Client:
+    """Fungsi untuk memanggil client Supabase secara global"""
+    if not SUPABASE_URL or not SUPABASE_KEY:
+        # Ini untuk handle jika lupa pasang Environment Variables
+        raise ValueError("Supabase URL atau Key belum dikonfigurasi!")
+        
+    return create_client(SUPABASE_URL, SUPABASE_KEY)
